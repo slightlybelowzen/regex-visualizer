41c164c6aafba0f954b36073ff9954398849e9ef
Prakhar Nagpal <prakhar.nagpal03@gmail.com>
2024-11-18 12:54:54 +0000

    fixed for mobile

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -29,7 +29,7 @@
 
 <main class="flex flex-col items-center justify-center">
 	<header class="text-center">
-		<h1 class="mb-2 text-3xl font-semibold">Visualising a regex engine</h1>
+		<h1 class="mb-2 text-2xl font-semibold md:text-3xl">Visualising a regex engine</h1>
 		<p class="text-zinc-400">
 			Something I've wanted to do for a while, combining writing a simple compiler (yes, regex
 			engines are simple compilers) and visualising the various stages of the output. I've only
@@ -38,23 +38,23 @@
 			,<code>?</code>
 		</p>
 	</header>
-	<form onsubmit={parseRegex} class="mt-12 flex flex-col items-center gap-4">
-		<div class="flex gap-6">
+	<form onsubmit={parseRegex} class="mt-8 flex flex-col items-center gap-4">
+		<div class="flex flex-col gap-4 md:flex-row md:gap-6">
 			<div>
-				<label for="regex">Regex input</label>
-				<input type="text" name="regex" class="mb-4" bind:value={regex} placeholder="Regex" />
+				<label for="regex">Regex</label>
+				<input type="text" name="regex" bind:value={regex} placeholder="regex" />
 			</div>
 			<div>
-				<label for="input">String input (to match against)</label>
-				<input type="text" name="input" bind:value={input} placeholder="String to match on" />
+				<label for="input">Input</label>
+				<input type="text" name="input" bind:value={input} placeholder="string to match on" />
 			</div>
 		</div>
-		<button type="submit">Visualize</button>
+		<button class="mt-2" type="submit">Visualize</button>
 	</form>
 	{#if context}
 		<RegexVisualizer {context} />
 	{/if}
-	<p class="mt-12 border-l-4 border-amber-700 bg-amber-700/10 px-1 py-2 text-center text-zinc-400">
+	<p class="mt-8 border-l-4 border-amber-700 bg-amber-700/10 px-1 py-2 text-center text-zinc-400">
 		PS. It's nowhere near finished, I'm still working on making this visualizer prettier and adding
 		the time travelling evaluation engine.
 	</p>
